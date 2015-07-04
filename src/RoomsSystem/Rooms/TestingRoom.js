ddb7b97c8961032bff462f9d4e81f034f0dd1fef
Francesco Negri <francesco.negri.98@outlook.com>
2015-07-04 12:02:49 +0200

    fix

--- src/RoomsSystem/Rooms/TestingRoom.js
+++ src/RoomsSystem/Rooms/TestingRoom.js
@@ -11,8 +11,15 @@ TestingRoom.prototype.constructor = TestingRoom;
 TestingRoom.prototype.preload = function () {
     BaseRoom.prototype.preload.call(this);
 
+    gameInstance.phaserGame.world.setBounds(0, 0, gameInstance.world.width, gameInstance.world.height);
+    this.add.existing(gameInstance.cDepth.depthGroup);
+    gameInstance.phaserGame.time.advancedTiming = true;
+
     gameInstance.phaserGame.load.crossOrigin = 'anonymous';
     gameInstance.loadImage('player', 'https://s3-eu-west-1.amazonaws.com/lbbucket/Assets/Images/player.png');
+
+    eurecaServer.clientHandler({ 'event': 'ready', 'params': { 'id': myId } });
+    console.log('Joining...');
 }
 
 TestingRoom.prototype.create = function() {
@@ -22,4 +29,12 @@ TestingRoom.prototype.create = function() {
     gameInstance.phaserGame.camera.follow(gameInstance.clientsList[myId]);
 
     gameInstance.cDepth.depthSort(gameInstance);
+
+    fpsText = gameInstance.phaserGame.add.text(10, 10, 'FPS: ' + gameInstance.phaserGame.time.fps);
+}
+
+var fpsText;
+
+GameState.prototype.update = function () {
+    fpsText.setText('FPS: ' + gameInstance.phaserGame.time.fps);
 }
