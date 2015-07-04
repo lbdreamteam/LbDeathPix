f342a46efb7115c84b6d4cabcb7658ca39369f2b
Francesco Negri <francesco.negri.98@outlook.com>
2015-07-04 10:44:11 +0200

    fix main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,68 @@
 ﻿var jsonMap; //TODO: rivedere questa variabile, non mi piace globale!
-gameInstance = new LBGame(1280, 720, 1500, 720, 32, true, true, Phaser.AUTO, 5);
+eurecaClient = new Eureca.Client();
+eurecaClient.ready(function (proxy) {
+    eurecaServer = proxy;
+});
+
+gameInstance = new LBGame(
+    800,    //width
+    600,    //height
+    2500,   //wWidth
+    600,    //wHeight
+    32,     //MovgridS
+    true,   //movIn8Dir
+    true,   //overlap
+    Phaser.AUTO,    //renderer
+    [       //pHs        
+        {
+            'event': 'createGame',
+            'params': ['id', 'Tx', 'Ty'],
+            'function': function (params) {
+                console.log('Creating game');
+                myId = params.id;
+                gameInstance.playerSpawnPoint = { x: params.Tx, y: params.Ty };
+                gameInstance.otherPlayersW.worker.postMessage({ event: 'init', params: myId }); //inizializza il worker
+                create();
+            }
+        },
+        {
+            'event': 'updatePlayer',
+            'params': ['x', 'y', 'callId'],
+            'function': function (params) {
+                gameInstance.clientsList[myId].updatePosition(params.x, params.y, params.callId);
+            }
+        },
+        {
+            'event': 'updateOtherPlayers',
+            'params': ['posTable'],
+            'function': function (params) {
+                otherPlayersManager.update(params.posTable);
+            }
+        },
+        {
+            'event': 'onOtherPlayerConnect',
+            'params': ['id', 'oldpos', 'nowPos'],
+            'function': function (params) {
+                console.log('On other player connect');
+                otherPlayersManager.onConnect(params.id, params.oldPos, params.nowPos);
+            }
+        },
+        {
+            'event': 'onOtherPlayerDisconnect',
+            'params': ['id'],
+            'function': function (params) {
+                otherPlayersManager.onDisconnect(params.id);
+            }
+        },
+        {
+            'event': 'spawnOtherPlayers',
+            'params': ['posTable'],
+            'function': function (params) {
+                otherPlayersManager.spawn(params.posTable);
+            }
+        }
+    ],
+    5);
 
 function preload() {
     gameInstance.setVisibilityChangeHandlers();
